fdfa878d74eb463a031063def383aaa3db57fe73
TravisKnop <travisknop@gmail.com>
2015-10-27 22:17:39 -0400

    initial page set up

--- urlshortener/shorturl/models.py
+++ urlshortener/shorturl/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+"""class ViewManager(models.Manager):
+
+    def filter_for_user(self, user):
+        return self.filter(author=user)
+"""
+
+class Homepage(models.Model):
+    # objects = ViewManager()
+
+    @property
+    def print_input(self):
+        return(input())
+
--- urlshortener/shorturl/views.py
+++ urlshortener/shorturl/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import DetailView, View, TemplateView
+from shorturl.models import Homepage
 
-# Create your views here.
+
+class HomeView(TemplateView):
+r    template_name = "base.html"
