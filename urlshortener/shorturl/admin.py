a6433fb96e06a6ab88580229fe0faa3b75070f0d
TravisKnop <travisknop@gmail.com>
2015-11-03 22:22:13 -0500

    trying to merge again

--- urlshortener/shorturl/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from shorturl.models import UrlRecord, UrlCounter, Profile
-
-admin.site.register(UrlRecord)
-admin.site.register(UrlCounter)
-admin.site.register(Profile)
--- urlshortener/shorturl/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-class InputModelTests(TestCase):
-
-    def initial_fail_test(self):
-        """The initial models function should receive an input and print it"""
-        self.fail("YES!")
