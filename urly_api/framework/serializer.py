adbb8322a9bad3c892fca5a10a9ba3a5ad22a3bc
TravisKnop <travisknop@gmail.com>
2015-11-10 21:13:54 -0500

    added initial framework and serializers

---
+++ urly_api/framework/serializer.py
@@ -0,0 +1,17 @@
+
+from rest_framework import serializers
+from api.models import UrlRecord, Profile
+
+
+class UrlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UrlRecord
+        field = ('long_url', 'short_url')
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        lookup_field = 'user'
+
+
